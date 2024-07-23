f2691eaa41cd77facd1644bb4525734b86406565
clencyc <clency2023@gmail.com>
2024-07-23 07:09:52 +0300

    Rest parameter syntax for functions

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,5 +1,5 @@
 // modify to return number of arguments passed to it using the rest parameter syntax
 
-export default function returnHowManyArguments() {
-
+export default function returnHowManyArguments(...theArgs) {
+  return theArgs.length;
 }
