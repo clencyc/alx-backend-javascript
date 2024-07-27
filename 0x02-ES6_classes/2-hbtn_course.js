504ce17025c9a63caf921ddb7998364e5f3e3450
clencyc <clency2023@gmail.com>
2024-07-27 17:24:41 +0300

    corrected code

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,49 +1,34 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    if (typeof name !== 'string') throw new TypeError('Name must be a string');
+    if (typeof length !== 'number') throw new TypeError('Length must be a number');
+
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
   get name() {
-    return this._name();
+    return this._name;
   }
 
-  set name(value) {
-    this._validateName(value);
-    this._name = value;
+  set name(name) {
+    this._name = name;
   }
 
   get length() {
     return this._length;
   }
 
-  set length(value) {
-    this._validateLength(value);
-    this._length = value;
+  set length(length) {
+    this._length = length;
   }
 
   get students() {
-    return [...this._students]; // Return a copy of the students array
+    return this._students;
   }
 
-  // No setter for students - it should not be modified after creation
-
-  _validateName(name) {
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-  }
-
-  _validateLength(length) {
-    if (typeof length !== 'number') {
-      throw new TypeError('Length must be a number');
-    }
-  }
-
-  _validateStudents(students) {
-    if (!Array.isArray(students) || students.some((student) => typeof student !== 'string')) {
-      throw new TypeError('Students   must be an array of strings');
-    }
+  set students(students) {
+    this._students = students;
   }
 }
