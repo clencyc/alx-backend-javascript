5c7ed57aef22cd64c896e346f8acf9bf4bbe6d31
clencyc <clency2023@gmail.com>
2024-07-30 00:23:20 +0300

    task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,15 +1,21 @@
-class Building {
+export default class Building {
   constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new Error('sqft must be a number');
+    }
     this._sqft = sqft;
+    this.evacuationWarningMessage();
   }
 
   get sqft() {
     return this._sqft;
   }
 
+  set sqft(value) {
+    this._sqft = value;
+  }
+
   evacuationWarningMessage() {
     throw new Error(`${this.constructor.name} must override evacuationWarningMessage`);
   }
 }
-
-export default Building;
