e0ff369bbd2b91790049f4d7c704d6e53f7e41a3
clencyc <clency2023@gmail.com>
2024-07-30 14:33:12 +0300

    get Ids

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,7 @@
+function getListStudentIds(students) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+  return students.map((students) => students.id);
+}
+export default getListStudentIds;
