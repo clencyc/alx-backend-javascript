68721e1758addc5b2937628fd357d7ef02ab2f80
clencyc <clency2023@gmail.com>
2024-08-02 08:11:38 +0300

    added major functions

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,11 +1,19 @@
 interface Student {
-    firstname: String;
-    lastname: String;
+    firstname: string;
+    lastname: string;
     age: number;
-    location: String;
+    location: string;
 }
 let student1 = new Student("Clency", "Christine", 17, "Nairobi");
 let student2 = new Student("Clency", "Christine", 17, "Nairobi");
 
 let StudentList: string[] = [student1, student2]
 
+function printTeacher(firstName: string, lastName: string) {
+    // returns the first letter of the firstName and the full lastName
+    
+}
+
+interface printTeacher {
+    
+}
