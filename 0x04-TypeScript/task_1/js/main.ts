ed73059740431c05ab09a84353e531e90971d554
clencyc <clency2023@gmail.com>
2024-08-02 07:37:42 +0300

    Task 1 files

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,39 @@
+interface Teacher {
+    firstName: String;
+    lastName: String;
+    fullTimeEmployee: boolean = false;
+    yearsOfExperience: number;
+    location: String;
+    contract: Boolean;
+}
+
+interface Directors extends Teacher {
+    numberOfReports: Number;
+}
+
+function printTeacher(firstName: String, lastName: String) {
+    // returns the first letter of the firstName and the full lastName
+    
+}
+
+interface printTeacher {
+    
+}
+
+class StudentClass {
+    constructor(
+    public fullName: string,
+    public lastName: string,
+    ) {
+
+    }
+    workOnHomework() {
+        return CurrentlyWorking: String;
+    }
+    displayName() {
+        return StudentClass.firstName;
+    }
+}
+
+// constructor of the class should be described through an interface 
+// the class should be described through an interface
