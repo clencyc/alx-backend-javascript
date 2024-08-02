2cc0c2db79f4cb52295923e71e6640e174eaebb1
clencyc <clency2023@gmail.com>
2024-08-02 07:38:12 +0300

    task 2 files

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,33 @@
+interface DirectorInterface {
+    workFromHome() {
+        return string;
+    }
+
+    getCoffeeBreak() {
+        return string;
+    }
+
+    workDirectorTasks() {
+        return string;
+    }
+}
+
+interface TeacherInterface {
+    workFromHome() {
+        return string;
+    }
+
+    getCoffeeBreak() {
+        return string;
+    }
+
+    workTeacherTasks() {
+        return string;
+    }
+}
+
+class Teacher {
+    TeacherInterface() {
+        
+    }
+}
