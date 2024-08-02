f7362421ee6e93e11ac3ed16650244891476a6ad
clencyc <clency2023@gmail.com>
2024-08-02 07:36:58 +0300

    added task 0 js file

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,11 @@
+interface Student {
+    firstname: String;
+    lastname: String;
+    age: number;
+    location: String;
+}
+let student1 = new Student("Clency", "Christine", 17, "Nairobi");
+let student2 = new Student("Clency", "Christine", 17, "Nairobi");
+
+let StudentList: string[] = [student1, student2]
+
